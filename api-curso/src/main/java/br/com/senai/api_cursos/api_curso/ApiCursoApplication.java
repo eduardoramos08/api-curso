e0a29c3c584494c53920ba512a79ffd7614fdfdc
eduardoramos08 <eduardoramos.senai@gmail.com>
2026-05-11 10:21:46 -0300

    Subindo API de cursos

---
+++ api-curso/src/main/java/br/com/senai/api_cursos/api_curso/ApiCursoApplication.java
@@ -0,0 +1,14 @@
+package br.com.senai.api_cursos.api_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+ApiCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursoApplication.class, args);
+	}
+
+}
---
+++ api-curso/src/test/java/br/com/senai/api_cursos/api_curso/ApiCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_cursos.api_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
