af2a10864065652b55c7a4735ea23ef85d7cf960
eduardoramos08 <eduardoramos.senai@gmail.com>
2026-05-17 19:50:46 -0300

    Documentação via Swagger finalizada

--- api-curso/src/main/java/br/com/senai/api_cursos/api_curso/controller/CursoController.java
+++ api-curso/src/main/java/br/com/senai/api_cursos/api_curso/controller/CursoController.java
@@ -33,35 +33,34 @@ public class CursoController {
 
     @PostMapping
     @Transactional
-    @Operation(summary = "Cadastrar um novo curso", description = "Salva um curso no Banco de Dados")
+    @Operation(summary = "Cadastrar um novo curso",description = "Salva um curso no Banco de Dados")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Curso cadastrado com sucesso",
             content = {
                     @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = DadosDetalhamentoCurso.class)
-                    )
-
-            }),
+                            )
+                    }
+            ),
             @ApiResponse(responseCode = "409", description = "Nome duplicado",
             content = {
                     @Content(
                             mediaType = "text/plain",
                             examples = @ExampleObject(value = "Já existe um curso com esse nome")
-                    )
-            })
+                            )
+                    }
+            ),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos")
     })
-
     public ResponseEntity<?> cadastrarCurso(
             @RequestBody @Valid DadosCadastroCurso dados
     ) {
-        // verifica nome duplicado
         if (repository.existsByNome(dados.nome())) {
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body("Já existe um curso com esse nome");
         }
         var curso = repository.save(new Curso(dados));
-
         return ResponseEntity
                 .created(URI.create("/cursos/" + curso.getId()))
                 .body(new DadosDetalhamentoCurso(curso));
@@ -69,46 +68,46 @@ public class CursoController {
 
 
     @GetMapping
-    @Operation(summary = "Listar cursos", description = "Lista todos os cursos que estão cadastrados no Banco de Dados")
+    @Operation(summary = "Listar cursos", description = "Lista todos os cursos cadastrados")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Lista de cursos",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = DadosListagemCurso.class)
-                            )
-                    }),
-            @ApiResponse(responseCode = "404", description = "Curso não encontrado", content = {@Content})
+            content = {
+                    @Content(mediaType = "application/json")
+                    }
+            )
     })
     public ResponseEntity<Page<DadosListagemCurso>> listarCursos(
             @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao
     ) {
+
         var page = repository.findAllByAtivoTrue(paginacao)
                 .map(DadosListagemCurso::new);
 
         return ResponseEntity.ok(page);
     }
 
-
     @GetMapping("/{id}")
     @Operation(summary = "Buscar curso pelo ID", description = "Busca os detalhes do curso especificado")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Curso encontrado",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = DadosDetalhamentoCurso.class)
+            content = {
+                      @Content(mediaType = "application/json",
+                              schema = @Schema(implementation = DadosDetalhamentoCurso.class)
                             )
-                    }),
-            @ApiResponse(responseCode = "404", description = "Curso não encontrado", content = {@Content})
+                    }
+            ),
+            @ApiResponse(responseCode = "404", description = "Curso não encontrado")
     })
     public ResponseEntity<DadosDetalhamentoCurso> detalharCurso(
             @PathVariable Long id
     ) {
+
         var curso = repository.findByIdAndAtivoTrue(id);
+
         if (curso.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
+
         return ResponseEntity.ok(
                 new DadosDetalhamentoCurso(curso.get())
         );
@@ -116,14 +115,16 @@ public class CursoController {
 
 
     @GetMapping("/periodos")
-    @Operation(summary = "Listar periodos", description = "Lista todos os periodos disponiveis")
+    @Operation(summary = "Listar períodos", description = "Lista todos os períodos disponíveis")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de periodos",
+            @ApiResponse(responseCode = "200", description = "Lista de períodos",
                     content = {
                             @Content(
                                     mediaType = "application/json"
                             )
-                    })
+                    }
+            )
+
     })
     public ResponseEntity<List<Curso.Periodo>> listarPeriodos() {
 
@@ -135,7 +136,8 @@ public class CursoController {
 
 
     @PutMapping
-    @Operation(summary = "Atualizar curso", description = "Atualiza curso especificado")
+    @Transactional
+    @Operation(summary = "Atualizar curso", description = "Atualiza o curso especificado")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Curso atualizado com sucesso",
                     content = {
@@ -143,30 +145,31 @@ public class CursoController {
                                     mediaType = "application/json",
                                     schema = @Schema(implementation = DadosDetalhamentoCurso.class)
                             )
-                    }),
+                    }
+            ),
+            @ApiResponse(responseCode = "404", description = "Curso não encontrado"),
             @ApiResponse(responseCode = "409", description = "Nome duplicado",
                     content = {
                             @Content(
                                     mediaType = "text/plain",
                                     examples = @ExampleObject(value = "Já existe um curso com esse nome")
                             )
-                    })
+                    }
+            ),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos")
     })
-    @Transactional
     public ResponseEntity<?> atualizarCurso(
             @RequestBody @Valid DadosAtualizarCurso dados
     ) {
 
         var optionalCurso = repository.findByIdAndAtivoTrue(dados.id());
 
-        // curso não encontrado
         if (optionalCurso.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
         var curso = optionalCurso.get();
 
-        // nome duplicado
         if (dados.nome() != null
                 && repository.existsByNome(dados.nome())
                 && !curso.getNome().equals(dados.nome())) {
@@ -182,25 +185,24 @@ public class CursoController {
         );
     }
 
-
     @DeleteMapping("/{id}")
-    @Operation(summary = "Excluir curso", description = "Excluir curso especificado pelo ID")
+    @Transactional
+    @Operation(summary = "Excluir curso", description = "Exclui o curso especificado pelo ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Curso excluido com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Curso excluído com sucesso"),
             @ApiResponse(responseCode = "404", description = "Curso não encontrado")
     })
-    @Transactional
     public ResponseEntity<Void> excluirCurso(
             @PathVariable Long id
     ) {
+
         var optionalCurso = repository.findByIdAndAtivoTrue(id);
 
-        // curso não encontrado
         if (optionalCurso.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        var curso = optionalCurso.get();
-        curso.excluirCurso();
+
+        optionalCurso.get().excluirCurso();
 
         return ResponseEntity.noContent().build();
     }
